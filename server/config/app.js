c607cfaa733530f3367c92ab055f1b0cf54ea835
Kim Nguyen <kimnguyen416@yahoo.com>
2017-09-21 12:51:45 -0400

    add https

--- server/config/app.js
+++ server/config/app.js
@@ -5,6 +5,6 @@ var config = require('./config'),
 module.exports.start = function() {
   var app = express.init();
   app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("should run");
   });
 };
